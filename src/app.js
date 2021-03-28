0d0a5d0eb9b02f6bcc62e94e82667e89513b0065
yusunjo <tnswh2023@gmail.com>
2021-03-28 22:47:07 +0900

    ejs setting, ejs index page, header, footer page

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@
 // https://www.daleseo.com/js-babel-node/  여기 보고 적용함
 
 import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
 
 import mainRouter from './routes/main.routes';
+import login from './routes/login/loginPage';
 
 const app = express();
 const port = 8000;
 
+app.use(express.static('template'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use('/', mainRouter);
+app.use('/', login);
 
 app.listen(port, () => {
   console.log(`
